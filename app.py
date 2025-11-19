ab0b74ccba2bb392f65e0a73201bd302549b0b3f
joya1704-cpu <joya1704@gmail.com>
2025-11-19 14:44:25 -0600

    Change app port from 5000 to 10000

--- app.py
+++ app.py
@@ -251,4 +251,4 @@ def dashboard():
 # EJECUCIÓN LOCAL
 # =========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
